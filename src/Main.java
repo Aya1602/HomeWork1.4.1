d603990248723e07b19004bd25b55d8fb6428f23
Aya1602 <aygerim_m1602@mail.ru>
2019-09-10 22:58:34 +0600

    Переделанный вариант

--- src/Main.java
+++ src/Main.java
@@ -2,99 +2,95 @@ import java.util.Random;
 
 public class Main {
 
-    public static int[] health = {700, 250, 250, 250, 250};
-    public static int[] hits = {50, 20, 20, 20, 15};
-    public static String[] hitTypes = {"Physical", "Physical",
-            "Magical", "Mental", "Medic"};
+    public static int[] health = {700, 250, 250, 250, 200};
+    public static int[] damage = {50, 20, 20, 20, 0};
+    public static String[] hitTypes = {"Physical", "Physical", "Magical", "Mental", "Medical"};
 
     public static void main(String[] args) {
-        printStatistic();
+        printStatistics();
         while (!isFinished()) {
-            changeBossDefence();
-            round();
-            printStatistic();
-
-
+            hitTypes[0] = genarateBossDefenceType();
+            gameRounddd();
+            heal();
         }
     }
 
-    public static int healingPlayer (int indexPlayer) {
-
-        if (health [indexPlayer] > 0 && health [4] > 0) {
-            return hits [4] + health [indexPlayer];
-        }
-
-        return health [indexPlayer];
-    }
-
-    public static void changeBossDefence() {
-        Random r = new Random();
-        int randomNumber = r.nextInt(3) + 1;
-        hitTypes[0] = hitTypes[randomNumber];
-    }
-
-    public static void round() {
-        for (int i = 1; i <= 4; i++) {
-            int healthRemain = bossHit(i);
-            if (healthRemain < 0) {
-                health[i] = 0;
-            } else {
-                health[i] = healthRemain;
+    public static void heal() {
+        if (health[4] > 0) {
+            if (health[1] > 0 && health[2] > 0 && health[3] > 0) {
+                for (int i = 1; i <= 3; i++) {
+                    health[i] = health[i] + 20;
+                }
             }
         }
-        for (int i = 1; i <= 3; i++) {
-            int healthRemain = playerHit(i);
-            if (healthRemain < 0) {
-                health[0] = 0;
-            } else {
-                health[0] = healthRemain;
-            }
-
-        }
-
-        for (int u = 1; u <= 3; u++) {
-            health [u] = healingPlayer(u);
-
-        }
-    }
-
-    public static int playerHit(int playerIndex) {
-        if (hitTypes[0].equals(hitTypes[playerIndex])) {
-            Random r = new Random();
-            int randomNumber = r.nextInt(7) + 2;
-            System.out.println(hitTypes[playerIndex]
-                    + " critically hits Boss " +
-                    hits[playerIndex] * randomNumber);
-            return health[0] - hits[playerIndex] * randomNumber;
-        } else {
-            return health[0] - hits[playerIndex];
-        }
-    }
-
-    public static int bossHit(int playerIndex) {
-        return health[playerIndex] - hits[0];
     }
 
     public static boolean isFinished() {
         if (health[0] <= 0) {
-            System.out.println("Heroes won the game!!!");
+            System.out.println("Герои won!!!");
             return true;
         }
-        if (health[1] <= 0 && health[2] <= 0 && health[3] <= 0) {
-            System.out.println("Boss won the game!!!");
+        if (health[1] <= 0 && health[2] <= 0 && health[3] <= 0 && health[4] <= 0) {
+            System.out.println("Boss won!!!");
             return true;
         }
         return false;
     }
 
-    public static void printStatistic() {
-        System.out.println("________________");
-        System.out.println("Boss health: " + health[0]);
-        System.out.println("Boss defence: " + hitTypes[0]);
-        System.out.println("Warrior health: " + health[1]);
-        System.out.println("Magic health: " + health[2]);
-        System.out.println("Mental health: " + health[3]);
-        System.out.println("________________");
+    public static void gameRounddd() {
+        for (int i = 1; i <= 4; i++) {
+            int healthRemainBoss = hitBoss(i);
+            if (healthRemainBoss < 0) {
+                health[0] = 0;
+            } else {
+                health[0] = healthRemainBoss;
+            }
+            int healthRemainHeroes = hitPlayer(i);
+            if (healthRemainHeroes < 0) {
+                health[i] = 0;
+            } else {
+                health[i] = healthRemainHeroes;
+            }
+
+        }
+        printStatistics();
+    }
+
+    public static String genarateBossDefenceType(){
+        Random r = new Random();
+        int randomNumber = r.nextInt(3) + 1;
+        return hitTypes[randomNumber];
+    }
+
+    public static int hitBoss(int playerIndex) {
+
+        Random r = new Random();
+        int randomNumber = r.nextInt(8) + 1; // генерируем случайное число от 1 до 5
+        if (hitTypes[0].equals(hitTypes[playerIndex])) {
+            System.out.println(hitTypes[playerIndex] + " нанес критический удар " +
+                    damage[playerIndex] * randomNumber);
+            return health[0] - damage[playerIndex] * randomNumber;
+        }
+        return health[0] - damage[playerIndex];
+
+    }
+
+
+    public static int hitPlayer(int playerIndex) {
+        return health[playerIndex] - damage[0];
+    }
+
+    public static void printStatistics() {
+        System.out.println("_________________________");
+        System.out.println("Boss health " + health[0]);
+        System.out.println("Warrior health " + health[1]);
+        System.out.println("Magic health " + health[2]);
+        System.out.println("Kinetic health " + health[3]);
+        System.out.println("Medic health " + health[4]);
+        System.out.println("_________________________");
+
     }
 }
+
+
 
